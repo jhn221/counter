f3bf924dea02ac37333f6d797a04e2296e91ce56
janghanna <ewal4291@gmail.com>
2023-06-14 19:13:21 +0900

    feat : popularity, 평점 항목 추가

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
   final String backdrop_path, title, overview;
+  final num vote_average, popularity;
   const DetailScreen({
     super.key,
     required this.title,
     required this.backdrop_path,
     required this.overview,
+    required this.vote_average,
+    required this.popularity,
   });
 
   @override
@@ -31,18 +34,32 @@ class DetailScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(29)),
-                  child: Image.network(
-                      'https://image.tmdb.org/t/p/w500$backdrop_path')),
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(29)),
+                clipBehavior: Clip.hardEdge,
+                child: Image.network(
+                    'https://image.tmdb.org/t/p/w500$backdrop_path'),
+              ),
               const SizedBox(
                 height: 20,
               ),
               Text(
                 title,
-                style: const TextStyle(fontSize: 25, color: Colors.black),
+                style: const TextStyle(fontSize: 40, color: Colors.black),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'movie rating : $vote_average',
+                style: const TextStyle(fontSize: 20, color: Colors.black),
+              ),
+              Text(
+                'popularity : $popularity',
+                style: const TextStyle(fontSize: 20, color: Colors.black),
               ),
               const Spacer(),
               const Text(
